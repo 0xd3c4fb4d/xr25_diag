0de100b39eff3ec2b40a06fb4a375520f649e071
Javier Lopez <j.lopez@microsyscom.com>
2016-08-22 19:31:48 +0200

    - XR25streamreader uses thread for non-blocking frame read - initial Fenix3parser
    Use pthread_cancel() for XR25streamreader thread cancellation

--- XR25streamreader.cc
+++ XR25streamreader.cc
@@ -14,7 +14,17 @@
  */
 
 #include "XR25streamreader.hh"
+#include <condition_variable>
+#include <tuple>
+#include <mutex>
 
+/** Frame received handler.
+ * @param parser The XR25frameparser to use
+ * @param c Translated frame (&quot;0xff 0xff&quot; replaced by &quot;0xff
+ *     &quot;)
+ * @param length Length of the frame in octets
+ * @param fra Reference to the parsed frame
+ */
 void XR25streamreader::frame_recv(XR25frameparser &parser,
 				  const unsigned char c[], int length,
 				  XR25frame &fra) {
@@ -26,12 +36,31 @@ void XR25streamreader::frame_recv(XR25frameparser &parser,
 void XR25streamreader::read_frames(XR25frameparser &parser) {
 	unsigned char frame[128] = { 0xff, 0x00 }, c, *p = &frame[1];
 	XR25frame fra;
+	std::condition_variable term;
+	std::mutex              term_m;
+	std::atomic_int         count(0);
+	// thread that updates __fra_sec once a second
+	std::thread stat_thread([&term, &term_m, &count, this]() {
+			std::unique_lock<std::mutex> lock(term_m);
+			while (term.wait_for(lock, std::chrono::seconds(1))
+			       == std::cv_status::timeout)
+				this->__fra_sec = count.exchange(0); });
+	
+	// thread cancellation clean-up handler
+	typedef std::tuple<std::condition_variable&, std::thread &> arg_tuple_t;
+	auto args = std::make_tuple(std::ref(term), std::ref(stat_thread));
+	pthread_cleanup_push([](void *p) {
+			auto args = *static_cast<arg_tuple_t *>(p);
+			std::get<0>(args).notify_one();
+			std::get<1>(args).join();
+		}, &args);
+	
 	while (!__in.eof()) {
 		if ((c = __in.get()) == 0xff) {
 			if ((c = __in.get()) == 0x00) { /* start of frame */
 				if (__synchronized)
 					frame_recv(parser, frame, p - frame,
-						   fra);
+						   fra), count++;
 				__synchronized = 1, p = &frame[1];
 			} else if (c != 0xff) /* translate 'ff ff' to 'ff' */
 				__in.unget();
@@ -39,6 +68,7 @@ void XR25streamreader::read_frames(XR25frameparser &parser) {
 
 		if (__synchronized)
 			(p - frame) < ARRAY_SIZE(frame) ? *p++ = c
-				      : __synchronized = 0;
+				      : __synchronized = 0, __sync_err_count++;
 	}
+	pthread_cleanup_pop(1);
 }
--- XR25streamreader.hh
+++ XR25streamreader.hh
@@ -17,10 +17,17 @@
 #define XR25STREAMREADER_HH
 
 #include <iostream>
+#include <atomic>
+#include <functional>
+#include <pthread.h>
+#include <thread>
 
-/* 0xff 0x00 0xaa 0xbb 0xcc 0xdd  .  .  .  0xff 0x00
- * |-------|   |   |    |    |---- byte 3
- *  header     |   |    |--------- byte 2
+/* XR25 frames start with 0xff 0x00; 0xff ocurrences in the frame sent on the
+ * wire as 0xff 0xff.
+ *
+ * 0xff 0x00 0xaa 0xbb 0xcc 0xdd  .  .  .  0xff 0x00  .  .  .
+ * |-------|   |   |    |    |---- byte 3  |-------|
+ *  header1    |   |    |--------- byte 2   header2
  *             |   |-------------- byte 1
  *             |------------------ byte 0
  */
@@ -29,11 +36,66 @@ struct XR25frame {
 	unsigned char calib_vrsn;   /* byte 1 */
 
 	unsigned char in_flags;     /* byte 2 */
-	
-	unsigned char out_flags;    /* byte 3 */
+#define IN_AC_REQUEST 0x02
+#define IN_AC_COMPRES 0x04
+#define IN_THROTTLE_0 0x08
+#define IN_NOT_PARKED 0x10
+#define IN_THROTTLE_1 0x20
 
+	unsigned char out_flags;    /* byte 3 */
+#define OUT_PUMP_ENABLE     0x01
+#define OUT_IDLE_REGULATION 0x02
+#define OUT_WASTEGATE_REG   0x04
+#define OUT_EGR_ENABLE      0x20
+#define OUT_CHECK_ENGINE    0x80
+	
 	int map;                    /* byte 4 */
-	int rpm;                    /* byte 5 */
+	int rpm;                    /* byte 5-6 */
+	int throttle;               /* byte 7 */
+	
+	unsigned char fault_flags_1; /* byte 8 */
+#define FAULT_MAP           0x04
+#define FAULT_SPD_SENSOR    0x10
+#define FAULT_LAMBDA_TMP    0x20
+#define FAULT_LAMBDA        0x80
+
+	unsigned char eng_pinging;   /* byte 9 */
+	int injection_us;            /* byte 10-11 */
+	int advance;                 /* byte 12 */
+	
+	unsigned char fault_flags_0; /* byte 14 */
+#define FAULT_WATER_OPEN_C  0x01
+#define FAULT_WATER_SHORT_C 0x02
+#define FAULT_AIR_OPEN_C    0x04
+#define FAULT_AIR_SHORT_C   0x08
+#define FAULT_TPS_LOW       0x40
+#define FAULT_TPS_HIGH      0x80
+	
+	unsigned char fault_fugitive; /* byte 15, see 'fault_flags_0' bits */
+	
+	unsigned char fault_flags_2; /* byte 16 */
+#define FAULT_EEPROM_CHECKSUM 0x20
+#define FAULT_PROG_CHECKSUM   0x80
+	
+	unsigned char fault_flags_4; /* byte 17 */
+#define FAULT_NOT_PUMP     0x01
+#define FAULT_NOT_WASTEGATE 0x04
+#define FAULT_NOT_EGR      0x08
+#define FAULT_NOT_IDLE_REG 0x20
+	
+	unsigned char fault_flags_3; /* byte 18 */
+#define FAULT_INJECTORS    0x10
+
+	float temp_water;            /* byte 19 */
+	float temp_air;              /* byte 20 */
+	float batt_v;                /* byte 21 */
+	float lambda_v;              /* byte 22 */
+	int idle_regulation;         /* byte 23 */
+	int idle_period;             /* byte 24 */
+	unsigned char eng_pinging_delay; /* byte 25 */
+	int atmos_pressure;          /* byte 26 */
+	unsigned char afr_correction;    /* byte 28 */
+	int spd_km_h;                /* byte 32 */
 };
 
 class XR25frameparser {
@@ -48,15 +110,43 @@ class XR25streamreader {
 private:
 	typedef void (*post_parse_t)(const unsigned char[], int, XR25frame &);
 
-	std::istream &__in;
-	bool          __synchronized;
-	post_parse_t  __post_parse;
+	std::istream     &__in;
+	std::atomic_bool __synchronized;
+	std::atomic_int  __sync_err_count, __fra_sec;
+	post_parse_t     __post_parse;
+	std::thread      *__thrd;
+	
 	void frame_recv(XR25frameparser &parser, const unsigned char[], int
 		, XR25frame &);
+	void read_frames(XR25frameparser &parser);
 public:
 	XR25streamreader(std::istream &s, post_parse_t p = nullptr)
-		: __in(s), __synchronized(0), __post_parse(p) {}
-	void read_frames(XR25frameparser &parser);
+		: __in(s), __synchronized(0),
+		  __sync_err_count(0), __fra_sec(0), __post_parse(p),
+		  __thrd(nullptr) {}
+	~XR25streamreader() { stop(); }
+
+	bool is_synchronized() { return __synchronized.load(); }
+	int  get_sync_err_count() { return __sync_err_count.load(); }
+	int  get_fra_per_sec() { return __fra_sec.load(); }
+	
+	/** Read frames non-blocking; call stop() to cancel thread
+	 * @param parser The XR25frameparser to use
+	 */
+	void start(XR25frameparser &parser) {
+		if (!__thrd)
+			__thrd = new std::thread([&parser, this]() {
+					this->read_frames(parser); });
+	}
+	
+	/** Stop internal thread; see start()
+	 */
+	inline void stop() {
+		if (__thrd) {
+			pthread_cancel(__thrd->native_handle()), __thrd->join();
+			delete __thrd;
+		}
+	}
 };
 
 #endif /* XR25STREAMREADER_HH */
